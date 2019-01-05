73886eff39e6e952e0c471518759ad2ed5e2c735
Lee Khai <leekhai18@gmail.com>
2019-01-05 11:47:01 +0700

    Init project with mockdata, auth0

---
+++ src/auth0-credentials.js
@@ -0,0 +1,4 @@
+module.exports = {
+  clientId: "iKhqYOD95G1dYgLs24rwb1ZiZmZX8qfg",
+  domain: "leekhai18.auth0.com"
+};
---
+++ src/data/category.model.js
@@ -0,0 +1,5 @@
+export default class Category {
+  cover: string;
+  category: string;
+  count: string;
+}
