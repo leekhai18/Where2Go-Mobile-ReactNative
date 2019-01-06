b74b00bac7facc3b277446475fa56348f5c0d7fc
Lee Khai <leekhai18@gmail.com>
2019-01-06 15:51:05 +0700

    Add Pagination Components with react-native-snap-carousel

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import {
-  Alert,
-  AppRegistry,
-  Button,
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  Image,
-  TouchableOpacity,
-  Dimensions
+	Alert,
+	AppRegistry,
+	Button,
+	Platform,
+	StyleSheet,
+	Text,
+	View,
+	ScrollView,
+	Image,
+	TouchableOpacity,
+	Dimensions
 } from 'react-native';
 import { getCategories, getItems } from './../../data/data';
 import Item from './../../data/item.model';
@@ -18,321 +18,276 @@ import Category from './../../data/category.model';
 import { Col, Row, Grid } from "react-native-easy-grid";
 
 export default class HomeScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedTabview: 0,
-      selectedTab: 0
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectedTabview: 0,
+			selectedTab: 0
+		}
+	}
 
-  componentDidMount() {
-    console.log(getCategories());
-    console.log(getItems());
-  }
+	componentDidMount() {
+		console.log(getCategories());
+		console.log(getItems());
+	}
 
-  showItem = (itemId) => {
-    console.log(`Tapped on ${itemId}`);
-    // this.routerExtensions.navigate(["detail/" + itemId, {
-    //   animated: true,
-    //   transition: {
-    //     name: "slideTop",
-    //     duration: 380,
-    //     curve: "easeIn"
-    //   }
-    // }]);
-  }
+	//Top nav bar tap methods
+	onBellTap = () => {
+		console.log('onBellTap');
+	}
 
-  toggleLike = (item) => {
-    item.isLike = !item.isLike;
-    if (item.isLike) {
-      item.likes += 1;
-    } else {
-      item.likes -= 1;
-    }
-  }
+	onSearchTap = () => {
+		console.log('onSearchTap');
+	}
 
-  toggleHeart = (item) => {
-    item.isFavorite = !item.isFavorite;
-  }
+	onAvatarTap = () => {
+		console.log('onAvatarTap');
+	}
 
-  categoryIcon = (itemCategory) => {
-    switch (itemCategory) {
-      case "Burger":
-        return String.fromCharCode(0xf0f5); //"fa-cutlery";
-        break;
-      case "Beer":
-        return String.fromCharCode(0xf0fc); //"fa-beer";
-        break;
-      case "Pancake":
-        return String.fromCharCode(0xf0f4); //"fa-coffee";
-        break;
-      case "Cake":
-        return String.fromCharCode(0xf1fd); //"fa-birthday-cake";
-        break;
-      default:
-        return String.fromCharCode(0xf06d); //"fa-fire";
-        break;
-    }
-  }
+	onPopularTap = () => {
+		this.setState({ selectedTabview: 0 });
+	}
 
-  //Top nav bar tap methods
-  onBellTap = () => {
-    console.log('onBellTap');
-  }
+	onCategoryTap = () => {
+		this.setState({ selectedTabview: 1 });
+	}
 
-  onSearchTap = () => {
-    console.log('onSearchTap');
-  }
+	onPromosTap = () => {
+		this.setState({ selectedTabview: 2 });
+	}
 
-  onAvatarTap = () => {
-    console.log('onAvatarTap');
-  }
+	//Bottom nav bar tap methods
+	onHomeTap = () => {
+		this.setState({ selectedTab: 0 });
+	}
 
-  onPopularTap = () => {
-    this.setState({ selectedTabview: 0 });
-  }
+	onWritePostTap = () => {
+		this.setState({ selectedTab: 1 });
+	}
 
-  onCategoryTap = () => {
-    this.setState({ selectedTabview: 1 });
-  }
+	onProfileTap = () => {
+		this.setState({ selectedTab: 2 });
+	}
 
-  onPromosTap = () => {
-    this.setState({ selectedTabview: 2 });
-  }
+	render() {
+		return (
+			<Grid>
+				<Row style={styles.navStatusBar}>
+					<Col size={5}>
+						<Text style={styles.statusTitle}>Home</Text>
+					</Col>
+					<Col size={1}>
+						<TouchableOpacity onPress={this.onSearchTap}>
+							<Image
+								style={styles.statusImage}
+								source={require('./../../../src/assets/images/search.png')}
+							/>
+						</TouchableOpacity>
+					</Col>
+					<Col size={1}>
+						<TouchableOpacity onPress={this.onBellTap}>
+							<Image
+								style={styles.statusImage}
+								source={require('./../../../src/assets/images/bell.png')}
+							/>
+						</TouchableOpacity>
+					</Col>
+					<Col size={1}>
+						<TouchableOpacity onPress={this.onAvatarTap}>
+							<Image
+								style={styles.statusAvatar}
+								source={require('./../../../src/assets/images/me.jpg')}
+							/>
+						</TouchableOpacity>
+					</Col>
+				</Row >
 
-  //Bottom nav bar tap methods
-  onHomeTap = () => {
-    this.setState({ selectedTab: 0 });
-  }
+				<Row style={{ backgroundColor: '#bd2122', height: 50 }}>
+					<Col style={{ borderBottomColor: '#ffffff', borderBottomWidth: 3, margin: 0 }}
+						onTouchStart={this.onPopularTap}>
+						{this.state.selectedTabview == 0 && (
+							<Col>
+								<Row>
+									<Image
+										style={styles.statusImage}
+										source={require('./../../../src/assets/images/popular.png')}>
+									</Image>
+								</Row>
+								<Row>
+									<Text>POPULAR</Text>
+								</Row>
+							</Col>
+						)}
+						{this.state.selectedTabview != 0 && (
+							<Row>
+								<Text>POPULAR</Text>
+							</Row>
+						)}
+					</Col>
 
-  onWritePostTap = () => {
-    this.setState({ selectedTab: 1 });
-  }
+					<Col style={{ borderBottomColor: '#ffffff', borderBottomWidth: 3, margin: 0 }}
+						onTouchStart={this.onCategoryTap}>
+						{this.state.selectedTabview == 1 && (
+							<Col>
+								<Row>
+									<Image
+										style={styles.statusImage}
+										source={require('./../../../src/assets/images/popular.png')}>
+									</Image>
+								</Row>
+								<Row>
+									<Text>POPULAR</Text>
+								</Row>
+							</Col>
+						)}
+						{this.state.selectedTabview != 1 && (
+							<Row>
+								<Text>POPULAR</Text>
+							</Row>
+						)}
+					</Col>
 
-  onProfileTap = () => {
-    this.setState({ selectedTab: 2 });
-  }
+					<Col style={{ borderBottomColor: '#ffffff', borderBottomWidth: 3, margin: 0 }}
+						onTouchStart={this.onPromosTap}>
+						{this.state.selectedTabview == 2 && (
+							<Col>
+								<Row>
+									<Image
+										style={styles.statusImage}
+										source={require('./../../../src/assets/images/popular.png')}>
+									</Image>
+								</Row>
+								<Row>
+									<Text>POPULAR</Text>
+								</Row>
+							</Col>
+						)}
+						{this.state.selectedTabview != 2 && (
+							<Row>
+								<Text>POPULAR</Text>
+							</Row>
+						)}
+					</Col>
+				</Row>
 
-  render() {
-    return (
-      <Grid>
-        <Row style={styles.navStatusBar}>
-          <Col size={5}>
-            <Text style={styles.statusTitle}>Home</Text>
-          </Col>
-          <Col size={1}>
-            <TouchableOpacity onPress={this.onSearchTap}>
-              <Image
-                style={styles.statusImage}
-                source={require('./../../../src/assets/images/search.png')}
-              />
-            </TouchableOpacity>
-          </Col>
-          <Col size={1}>
-            <TouchableOpacity onPress={this.onBellTap}>
-              <Image
-                style={styles.statusImage}
-                source={require('./../../../src/assets/images/bell.png')}
-              />
-            </TouchableOpacity>
-          </Col>
-          <Col size={1}>
-            <TouchableOpacity onPress={this.onAvatarTap}>
-              <Image
-                style={styles.statusAvatar}
-                source={require('./../../../src/assets/images/me.jpg')}
-              />
-            </TouchableOpacity>
-          </Col>
-        </Row >
+				<ScrollView>
+					<Row>
+						<Col style={{ backgroundColor: '#81ecec' }}></Col>
+						<Col style={{ backgroundColor: '#6c5ce7' }}></Col>
+						<Col style={{ backgroundColor: '#ff7675' }}></Col>
+					</Row>
+				</ScrollView>
 
-        <Row style={{ backgroundColor: '#bd2122', height: 50 }}>
-          <Col style={{ borderBottomColor: '#ffffff', borderBottomWidth: 3, margin: 0 }}
-            onTouchStart={this.onPopularTap}>
-            {this.state.selectedTabview == 0 && (
-              <Col>
-                <Row>
-                  <Image
-                    style={styles.statusImage}
-                    source={require('./../../../src/assets/images/popular.png')}>
-                  </Image>
-                </Row>
-                <Row>
-                  <Text>POPULAR</Text>
-                </Row>
-              </Col>
-            )}
-            {this.state.selectedTabview != 0 && (
-              <Row>
-                <Text>POPULAR</Text>
-              </Row>
-            )}
-          </Col>
+				<Row style={{ height: 50 }}>
+					<Col onTouchStart={this.onHomeTap}>
+						{this.state.selectedTab == 0 && (
+							<Col>
+								<Row>
+									<Image
+										style={styles.statusImage}
+										source={require('./../../../src/assets/images/navhomem.png')}>
+									</Image>
+								</Row>
+								<Row>
+									<Text>Địa điểm</Text>
+								</Row>
+							</Col>
+						)}
+						{this.state.selectedTab != 0 && (
+							<Row>
+								<Image
+									style={styles.statusImage}
+									source={require('./../../../src/assets/images/navhome.png')}>
+								</Image>
+							</Row>
+						)}
+					</Col>
 
-          <Col style={{ borderBottomColor: '#ffffff', borderBottomWidth: 3, margin: 0 }}
-            onTouchStart={this.onCategoryTap}>
-            {this.state.selectedTabview == 1 && (
-              <Col>
-                <Row>
-                  <Image
-                    style={styles.statusImage}
-                    source={require('./../../../src/assets/images/popular.png')}>
-                  </Image>
-                </Row>
-                <Row>
-                  <Text>POPULAR</Text>
-                </Row>
-              </Col>
-            )}
-            {this.state.selectedTabview != 1 && (
-              <Row>
-                <Text>POPULAR</Text>
-              </Row>
-            )}
-          </Col>
+					<Col onTouchStart={this.onWritePostTap}>
+						{this.state.selectedTab == 1 && (
+							<Col>
+								<Row>
+									<Image
+										style={styles.statusImage}
+										source={require('./../../../src/assets/images/navordersm.png')}>
+									</Image>
+								</Row>
+								<Row>
+									<Text>Viết bài</Text>
+								</Row>
+							</Col>
+						)}
+						{this.state.selectedTab != 1 && (
+							<Row>
+								<Image
+									style={styles.statusImage}
+									source={require('./../../../src/assets/images/navorders.png')}>
+								</Image>
+							</Row>
+						)}
+					</Col>
 
-          <Col style={{ borderBottomColor: '#ffffff', borderBottomWidth: 3, margin: 0 }}
-            onTouchStart={this.onPromosTap}>
-            {this.state.selectedTabview == 2 && (
-              <Col>
-                <Row>
-                  <Image
-                    style={styles.statusImage}
-                    source={require('./../../../src/assets/images/popular.png')}>
-                  </Image>
-                </Row>
-                <Row>
-                  <Text>POPULAR</Text>
-                </Row>
-              </Col>
-            )}
-            {this.state.selectedTabview != 2 && (
-              <Row>
-                <Text>POPULAR</Text>
-              </Row>
-            )}
-          </Col>
-        </Row>
-
-        <ScrollView>
-          <Row>
-            <Col style={{ backgroundColor: '#81ecec' }}></Col>
-            <Col style={{ backgroundColor: '#6c5ce7' }}></Col>
-            <Col style={{ backgroundColor: '#ff7675' }}></Col>
-          </Row>
-        </ScrollView>
-
-        <Row style={{ height: 50 }}>
-          <Col onTouchStart={this.onHomeTap}>
-            {this.state.selectedTab == 0 && (
-              <Col>
-                <Row>
-                  <Image
-                    style={styles.statusImage}
-                    source={require('./../../../src/assets/images/navhomem.png')}>
-                  </Image>
-                </Row>
-                <Row>
-                  <Text>Địa điểm</Text>
-                </Row>
-              </Col>
-            )}
-            {this.state.selectedTab != 0 && (
-              <Row>
-                <Image
-                  style={styles.statusImage}
-                  source={require('./../../../src/assets/images/navhome.png')}>
-                </Image>
-              </Row>
-            )}
-          </Col>
-
-          <Col onTouchStart={this.onWritePostTap}>
-            {this.state.selectedTab == 1 && (
-              <Col>
-                <Row>
-                  <Image
-                    style={styles.statusImage}
-                    source={require('./../../../src/assets/images/navordersm.png')}>
-                  </Image>
-                </Row>
-                <Row>
-                  <Text>Viết bài</Text>
-                </Row>
-              </Col>
-            )}
-            {this.state.selectedTab != 1 && (
-              <Row>
-                <Image
-                  style={styles.statusImage}
-                  source={require('./../../../src/assets/images/navorders.png')}>
-                </Image>
-              </Row>
-            )}
-          </Col>
-
-          <Col onTouchStart={this.onProfileTap}>
-            {this.state.selectedTab == 2 && (
-              <Col>
-                <Row>
-                  <Image
-                    style={styles.statusImage}
-                    source={require('./../../../src/assets/images/navusm.png')}>
-                  </Image>
-                </Row>
-                <Row>
-                  <Text>Trang cá nhân</Text>
-                </Row>
-              </Col>
-            )}
-            {this.state.selectedTab != 2 && (
-              <Row>
-                <Image
-                  style={styles.statusImage}
-                  source={require('./../../../src/assets/images/navus.png')}>
-                </Image>
-              </Row>
-            )}
-          </Col>
-        </Row>
-      </Grid >
-    );
-  }
+					<Col onTouchStart={this.onProfileTap}>
+						{this.state.selectedTab == 2 && (
+							<Col>
+								<Row>
+									<Image
+										style={styles.statusImage}
+										source={require('./../../../src/assets/images/navusm.png')}>
+									</Image>
+								</Row>
+								<Row>
+									<Text>Trang cá nhân</Text>
+								</Row>
+							</Col>
+						)}
+						{this.state.selectedTab != 2 && (
+							<Row>
+								<Image
+									style={styles.statusImage}
+									source={require('./../../../src/assets/images/navus.png')}>
+								</Image>
+							</Row>
+						)}
+					</Col>
+				</Row>
+			</Grid >
+		);
+	}
 }
 
 const dimensions = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-  navStatusBar: {
-    backgroundColor: '#bd2122',
-    height: 55,
-    paddingTop: 15
-  },
-  statusTitle: {
-    color: '#ffffff',
-    fontSize: 18,
-    marginLeft: 15,
-    marginTop: 8,
-  },
-  statusImage: {
-    marginTop: 4,
-    marginRight: 20,
-    width: 30,
-    height: 30,
-  },
-  statusAvatar: {
-    borderWidth: 1,
-    borderColor: '#ffffff',
-    backgroundColor: '#f1ebeb',
-    borderRadius: 15,
-    marginTop: 4,
-    marginRight: 15,
-    width: 30,
-    height: 30,
-    resizeMode: 'stretch',
-  },
-  navBottom: {
-    height: 50
-  }
+	navStatusBar: {
+		backgroundColor: '#bd2122',
+		height: 55,
+		paddingTop: 15
+	},
+	statusTitle: {
+		color: '#ffffff',
+		fontSize: 18,
+		marginLeft: 15,
+		marginTop: 8,
+	},
+	statusImage: {
+		marginTop: 4,
+		marginRight: 20,
+		width: 30,
+		height: 30,
+	},
+	statusAvatar: {
+		borderWidth: 1,
+		borderColor: '#ffffff',
+		backgroundColor: '#f1ebeb',
+		borderRadius: 15,
+		marginTop: 4,
+		marginRight: 15,
+		width: 30,
+		height: 30,
+		resizeMode: 'stretch',
+	},
+	navBottom: {
+		height: 50
+	}
 });
---
+++ src/screens/home/paginationListItems/index.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import { Platform, View, ScrollView, Text, StatusBar, SafeAreaView } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import Carousel, { Pagination } from 'react-native-snap-carousel';
+import { sliderWidth, itemWidth } from './styles/SliderEntry.style';
+import SliderEntry from './components/SliderEntry';
+import styles, { colors } from './styles/index.style';
+import { ENTRIES1 } from './static/entries';
+
+const IS_ANDROID = Platform.OS === 'android';
+const SLIDER_1_FIRST_ITEM = 1;
+
+export default class example extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			slider1ActiveSlide: SLIDER_1_FIRST_ITEM
+		};
+	}
+
+	_renderItem({ item, index }) {
+		return <SliderEntry data={item} even={(index + 1) % 2 === 0} />;
+	}
+
+	_renderItemWithParallax({ item, index }, parallaxProps) {
+		return (
+			<SliderEntry
+				data={item}
+				even={(index + 1) % 2 === 0}
+				parallax={true}
+				parallaxProps={parallaxProps}
+			/>
+		);
+	}
+
+	main(title) {
+		const { slider1ActiveSlide } = this.state;
+
+		return (
+			<View style={styles.exampleContainer}>
+				<Text style={styles.subtitle}>{title}</Text>
+				<Carousel
+					ref={c => this._slider1Ref = c}
+					data={ENTRIES1}
+					renderItem={this._renderItemWithParallax}
+					sliderWidth={sliderWidth}
+					itemWidth={itemWidth}
+					hasParallaxImages={true}
+					firstItem={SLIDER_1_FIRST_ITEM}
+					inactiveSlideScale={0.94}
+					inactiveSlideOpacity={0.7}
+					inactiveSlideShift={20}
+					containerCustomStyle={styles.slider}
+					contentContainerCustomStyle={styles.sliderContentContainer}
+					loop={true}
+					loopClonesPerSide={2}
+					autoplay={true}
+					autoplayDelay={500}
+					autoplayInterval={3000}
+					onSnapToItem={(index) => this.setState({ slider1ActiveSlide: index })}
+				/>
+				<Pagination
+					dotsLength={ENTRIES1.length}
+					activeDotIndex={slider1ActiveSlide}
+					containerStyle={styles.paginationContainer}
+					dotColor={'rgba(255, 255, 255, 0.92)'}
+					dotStyle={styles.paginationDot}
+					inactiveDotColor={colors.black}
+					inactiveDotOpacity={0.4}
+					inactiveDotScale={0.6}
+					carouselRef={this._slider1Ref}
+					tappableDots={!!this._slider1Ref}
+				/>
+			</View>
+		);
+	}
+
+	get gradient() {
+		return (
+			<LinearGradient
+				colors={[colors.background1, colors.background2]}
+				startPoint={{ x: 1, y: 0 }}
+				endPoint={{ x: 0, y: 1 }}
+				style={styles.gradient}
+			/>
+		);
+	}
+
+	render() {
+		const pagination = this.main('Default layout | Loop | Autoplay | Parallax | Scale | Opacity | Pagination with tappable dots');
+
+		return (
+			<SafeAreaView style={styles.safeArea}>
+				<View style={styles.container}>
+					<StatusBar
+						translucent={true}
+						backgroundColor={'rgba(0, 0, 0, 0.3)'}
+						barStyle={'light-content'}
+					/>
+					{this.gradient}
+					<ScrollView
+						style={styles.scrollview}
+						scrollEventThrottle={200}
+						directionalLockEnabled={true}
+					>
+						{pagination}
+					</ScrollView>
+				</View>
+			</SafeAreaView>
+		);
+	}
+}
